181196dfcd646e7c4dc630564b0a53d531bcf6b5
mcapace <mike.capace@gmail.com>
2025-12-02 11:08:52 -0500

    Remove redundant 'Proud Sponsors' text above logos

--- src/components/SponsorsSection.tsx
+++ src/components/SponsorsSection.tsx
@@ -6,9 +6,6 @@ export default function SponsorsSection() {
   return (
     <section className="py-12 sm:py-16 bg-white border-t border-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <h3 className="text-xs uppercase tracking-[3px] text-gray-500 font-semibold mb-8">
-          Proud Sponsors
-        </h3>
         <div className="flex justify-center items-center">
           <div className="relative w-full max-w-5xl h-auto">
             <Image
